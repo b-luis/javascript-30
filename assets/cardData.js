94a8a12360eb6c89b291d1579311ecbbcfb68817
b-luis <bscarletluis@gmail.com>
2023-08-06 14:48:24 +0800

    update: main page

---
+++ assets/cardData.js
@@ -0,0 +1,153 @@
+const cardData = [
+    {
+      title: "01 - JavaScript Drum Kit",
+      link: "../01 - JavaScript Drum Kit/index.html",
+      timestamp: "2023-08-04 03:57",
+    },
+    {
+      title: "02 - JS and CSS Clock",
+      link: "../02 - JS and CSS Clock/index.html",
+      timestamp: "2023-08-06 05:41",
+    },
+    {
+      title: "03 - CSS Variables",
+      link: "../03 - CSS Variables/index.html",
+      timestamp: "2023-08-06 06:37",
+    },
+    {
+      title: "04 - Array Cardio Day 1",
+      link: "../04 - Array Cardio Day 1/index.html",
+      timestamp: "2023-08-06 06:36",
+    },
+    {
+      title: "05 - Flex Panel Gallery",
+      link: "../05 - Flex Panel Gallery/index.html",
+      timestamp: "2023-08-06 06:36",
+    },
+    {
+      title: "06 - Type Ahead",
+      link: "../06 - Type Ahead/index.html",
+      timestamp: "2023-08-06 06:37",
+    },
+    {
+      title: "07 - Array Cardio Day 2",
+      link: "../07 - Array Cardio Day 2/index.html",
+      timestamp: "2023-08-06 06:37",
+    },
+    {
+      title: "08 - Fun with HTML5 Canvas",
+      link: "../08 - Fun with HTML5 Canvas/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "09 - Dev Tools Domination",
+      link: "../09 - Dev Tools Domination/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "10 - Hold Shift and Check Checkboxes",
+      link: "../10 - Hold Shift and Check Checkboxes/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "11 - Custom Video Player",
+      link: "../11 - Custom Video Player/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "12 - Key Sequence Detection",
+      link: "../12 - Key Sequence Detection/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "13 - Slide in on Scroll",
+      link: "../13 - Slide in on Scroll/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "14 - JavaScript References VS Copying",
+      link: "../14 - JavaScript References VS Copying/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "15 - LocalStorage",
+      link: "../15 - LocalStorage/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "16 - Mouse Move Shadow",
+      link: "../16 - Mouse Move Shadow/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "17 - Sort Without Articles",
+      link: "../17 - Sort Without Articles/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "18 - Adding Up Times with Reduce",
+      link: "../18 - Adding Up Times with Reduce/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "19 - Webcam Fun",
+      link: "../19 - Webcam Fun/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "20 - Speech Detection",
+      link: "../20 - Speech Detection/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "21 - Geolocation",
+      link: "..21 - Geolocation/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "22 - Follow Along Link Highlighter",
+      link: "../22 - Follow Along Link Highlighter/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "23 - Speech Synthesis",
+      link: "../23 - Speech Synthesis/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "24 - Sticky Nav",
+      link: "../24 - Sticky Nav/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "25 - Event Capture, Propagation, Bubbling and Once",
+      link: "../25 - Event Capture, Propagation, Bubbling and Once/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "26 - Stripe Follow Along Nav",
+      link: "../26 - Stripe Follow Along Nav/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "27 - Click and Drag",
+      link: "../27 - Click and Drag/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "28 - Video Speed Controller",
+      link: "../28 - Video Speed Controller/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "29 - Countdown Timer",
+      link: "./29 - Countdown Timer/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+    {
+      title: "30 - Whack A Mole",
+      link: "./30 - Whack A Mole/index.html",
+      timestamp: "2023-08-04 01:51",
+    },
+  ];
+  
